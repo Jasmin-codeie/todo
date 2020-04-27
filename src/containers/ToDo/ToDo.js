391127bdf52036f76a25b719093bd502f6186263
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-27 15:52:38 +0600

    Created ToDo component

---
+++ src/containers/ToDo/ToDo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from '*.module.css';
+
+export default() => {
+    return(
+        <div className={classes.ToDo}>
+            
+        </div>
+    )
+}
