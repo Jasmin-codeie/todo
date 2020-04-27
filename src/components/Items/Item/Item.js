b0eecb2f34649b8e078f05eba0ac078b2c921bfe
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-27 16:40:19 +0600

    Added completed function

--- src/components/Items/Item/Item.js
+++ src/components/Items/Item/Item.js
@@ -2,8 +2,22 @@ import React from "react";
 import classes from "./Item.module.css";
 import Button from "../../UI/Button/Button";
 
-export default ({ item, deleteItem }) => (
-<li className={classes.Item}> <span className={classes.text}> {item.text}</span>
-<Button click={deleteItem} red>x</Button>
-</li>)
-;
+export default ({ item, deleteItem, toggleCompleteItem }) => {
+  const itemClasses = [
+    classes.Item,
+    item.completed ? classes.completed : null,
+  ].join(" ");
+
+  return (
+    <li className={itemClasses}>
+      {" "}
+      <span onClick={toggleCompleteItem} className={classes.text}>
+        {" "}
+        {item.text}
+      </span>
+      <Button click={deleteItem} red>
+        x
+      </Button>
+    </li>
+  );
+};
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import classes from "./Items.module.css";
 import Item from "./Item/Item";
 
-export default ({items, deleteItem}) => {
+export default ({items, deleteItem, toggleCompleteItem}) => {
   const itemsOutput = Object.keys(items).map(id => {
-    return <Item key={id} item={items[id]} deleteItem={() => deleteItem(id)}  />;
+    return <Item key={id} item={items[id]} deleteItem={() => deleteItem(id)} toggleCompleteItem={() => toggleCompleteItem(id)} />;
   });
 
   return (
