429fb76a2e3669051bdbfe3fdecc8284321ae403
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-27 16:18:53 +0600

    Created Button component

--- src/components/Items/Item/Item.js
+++ src/components/Items/Item/Item.js
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./Item.module.css";
+import Button from "../../UI/Button/Button";
 
 export default ({ item }) => (
-<li className={classes.Item}> <span className={classes.text}> {item.text}</span></li>)
+<li className={classes.Item}> <span className={classes.text}> {item.text}</span>
+<Button red>x</Button>
+</li>)
 ;
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Button.module.css";
+
+export default ({children}) => (
+    <button className={classes.Button}>{children}</button>
+)
+;
