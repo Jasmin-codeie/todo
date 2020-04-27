66db695fa4f9ba5609781c7edec4d2f0c2a3b8a0
Jasmin-codeie <jasmin.shabieva@gmail.com>
2020-04-27 15:59:59 +0600

    Use state in ToDo

--- src/containers/ToDo/ToDo.js
+++ src/containers/ToDo/ToDo.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import classes from '*.module.css';
+import React, { useState } from "react";
+import classes from "./ToDo.module.css";
 
-export default() => {
-    return(
-        <div className={classes.ToDo}>
-            
-        </div>
-    )
-}
+export default () => {
+  const [items, setItems] = useState({
+    "todo-1": {
+      text: "Hello world",
+      completed: false,
+    },
+    "todo-2": {
+      text: "Bye world",
+      completed: true,
+    },
+  });
+
+  const itemsOutput = Object.keys(items).map((key) => {
+    return <li>{items[key].text}</li>;
+  });
+  return (
+    <div className={classes.ToDo}>
+      <ul>{itemsOutput}</ul>
+    </div>
+  );
+};
